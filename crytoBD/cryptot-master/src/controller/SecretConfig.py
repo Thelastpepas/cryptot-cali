9ca1fe98ef98e0fcf7319acf4b04adb8f732a3db
Andeerson <159457050+andersonmonsal@users.noreply.github.com>
2024-10-23 06:34:29 -0500

    Delete crytoBD/cryptot-master directory

--- crytoBD/cryptot-master/src/controller/SecretConfig.py
+++
@@ -1,6 +0,0 @@
-# Do not expose your Neon credentials to the browser
-
-PGHOST='ep-billowing-sound-a56q2fm7.us-east-2.aws.neon.tech'
-PGDATABASE='neondb'
-PGUSER='neondb_owner'
-PGPASSWORD='keNj0TpRK3aL'
